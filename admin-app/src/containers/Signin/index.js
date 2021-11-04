ec0cb06daaa7b3795270c9a422eab74d1beb1de0
thuongnguyen01685 <sinanhsinanh@gmail.com>
2021-11-04 23:10:59 +0700

    Admin Redux Store Setup

--- admin-app/src/containers/Signin/index.js
+++ admin-app/src/containers/Signin/index.js
@@ -2,14 +2,25 @@ import React from "react";
 import Layout from "../../components/Layout";
 import { Container, Form, Row, Col, Button } from "react-bootstrap";
 import Input from "../../components/UI/Input";
+import { login } from "../../actions";
+import { useDispatch } from "react-redux";
 
 const Signin = (props) => {
+  const dispatch = useDispatch();
+  const userLogin = (e) => {
+    e.preventDefault();
+    const user = {
+      email: "thuong@gmail.com",
+      password: "123456",
+    };
+    dispatch(login(user));
+  };
   return (
     <Layout>
       <Container>
         <Row style={{ marginTop: "50px" }}>
           <Col md={{ span: 6, offset: 3 }}>
-            <Form>
+            <Form onSubmit={userLogin}>
               <Input
                 label="Email Address"
                 placeholder="Email Address"
---
+++ admin-app/src/reducers/index.js
@@ -0,0 +1,9 @@
+import authReducers from "./auth.reducers";
+
+import { combineReducers } from "redux";
+
+const rootReducer = combineReducers({
+  auth: authReducers,
+});
+
+export default rootReducer;
