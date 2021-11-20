c2f76f41a29850b51ef53456b8078c3cd1a81041
thuongnguyen01685 <sinanhsinanh@gmail.com>
2021-11-20 16:01:19 +0700

    Header UI

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,11 +1,14 @@
 import categoryReducers from "./category.reducers";
 import productReducers from "./product.reducer";
 
+import authReducers from "./auth.reducers";
+
 import { combineReducers } from "redux";
 
 const rootReducer = combineReducers({
   category: categoryReducers,
   product: productReducers,
+  auth: authReducers,
 });
 
 export default rootReducer;
